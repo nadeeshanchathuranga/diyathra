323238240978c8ff10dad403e90d5cf26a745508
GeekMac <ashandevinda9485@gmail.com>
2026-04-04 12:57:52 +0530

    done of the day

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit6e7d38426435671d3299f9ebc0427c6b
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit6e7d38426435671d3299f9ebc0427c6b::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire6e7d38426435671d3299f9ebc0427c6b($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit6e7d38426435671d3299f9ebc0427c6b::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire6e7d38426435671d3299f9ebc0427c6b($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
--- vendor/inertiajs/inertia-laravel/src/Middleware.php
+++ vendor/inertiajs/inertia-laravel/src/Middleware.php
@@ -85,7 +85,7 @@ class Middleware
     /**
      * Define a callback that returns the relative URL.
      *
-     * @return \Closure|null
+     * @return Closure|null
      */
     public function urlResolver()
     {
@@ -95,7 +95,7 @@ class Middleware
     /**
      * Handle the incoming request.
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function handle(Request $request, Closure $next)
     {
@@ -122,6 +122,10 @@ class Middleware
         $response = $next($request);
         $response->headers->set('Vary', Header::INERTIA);
 
+        if ($response->isRedirect()) {
+            $this->reflash($request);
+        }
+
         if (! $request->header(Header::INERTIA)) {
             return $response;
         }
@@ -136,10 +140,6 @@ class Middleware
 
         if ($response->getStatusCode() === 302 && in_array($request->method(), ['PUT', 'PATCH', 'DELETE'])) {
             $response->setStatusCode(303);
-        }
-
-        if ($response->isRedirect()) {
-            $this->reflash($request);
         }
 
         return $response;
